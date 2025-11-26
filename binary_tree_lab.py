6e742545b6143e0ee944723a0a95d7908dd87b97
Nevin <n.boyd1@yahoo.com>
2025-11-26 07:14:24 -0800

    Implemented max_depth and lowest ancestor

--- binary_tree_lab.py
+++ binary_tree_lab.py
@@ -8,8 +8,18 @@ class TreeNode:
 
 # TODO: Implement the max_depth function
 def max_depth(root: Optional[TreeNode]) -> int:
-    pass
+    if not root:
+        return 0
+    return 1 + max(max_depth(root.left), max_depth(root.right))
+
 
 # TODO: Implement the lowest_common_ancestor function
 def lowest_common_ancestor(root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
-    pass
+    current = root
+    while current:
+        if p.val < current.val and q.val < current.val:
+            current = current.left
+        elif p.val > current.val and q.val > current.val:
+            current = current.right
+        else:
+            return current
